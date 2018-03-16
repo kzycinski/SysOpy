fe6619d0396880d6496687b57dc35fd3f305c3c4
legendar1o <krystian.zycinski@hotmail.com>
2018-03-16 16:49:09 +0100

    Added flags and comments

--- lab1/library.c
+++ lab1/library.c
@@ -8,14 +8,17 @@
 
 char static_array[STATIC_ARRAY_SIZE][STATIC_BLOCK_SIZE];
 
-
+//Creating structure with array, if is_static == 0 then memory is allocated dynamically
 struct array_struct *create_struct(int blocks, int block_size, int is_static){
     if(blocks <=0 || block_size <= 0) return NULL;
 
-    struct array_struct *arr_struct= malloc(sizeof(arr_struct));
+    struct array_struct *arr_struct = malloc(sizeof(arr_struct));
+
     arr_struct -> blocks = blocks;
     arr_struct -> block_size = block_size;
     arr_struct -> is_static = is_static;
+
+    //allocating array
     if(is_static)
         arr_struct -> array = (char **) static_array;
     else{
@@ -26,20 +29,23 @@ struct array_struct *create_struct(int blocks, int block_size, int is_static){
     return arr_struct;
 }
 
+//deleting array, firstly deleting all blocks then whole structure
 void delete_array(struct array_struct *arr_struct){
     for (int i = 0 ; i < arr_struct -> blocks ; i++){
         delete_block(arr_struct, i);
     }
 
+//if array is not allocated satically then free array memory
     if(!arr_struct->is_static) free(arr_struct->array);
     free(arr_struct);
 }
 
+//deleting single block
 void delete_block(struct array_struct *arr_struct, int index){
     if (index < 0 || index >= arr_struct -> blocks) return;
     if (arr_struct -> array[index] == NULL) return;
     if (arr_struct -> is_static) {
-        arr_struct->array[index] = "";
+        arr_struct->array[index] = ""; //if array is static then instead of deleting I just make the block empty
         return;
     }
     else {
@@ -48,6 +54,7 @@ void delete_block(struct array_struct *arr_struct, int index){
     return;
 }
 
+//adding block on exact index
 void add_block(struct array_struct *arr_struct, int index, char *block){
     if (index < 0 || index > arr_struct -> blocks) return;
     if (arr_struct -> array[index] != NULL) return;
@@ -58,6 +65,7 @@ void add_block(struct array_struct *arr_struct, int index, char *block){
         arr_struct -> array[index] = strcpy((malloc(arr_struct -> block_size * sizeof(char))), block);
 }
 
+//calculate ASCII sum of block
 int calculate_block(char *block){
     int tmp = 0;
     for (int i = 0 ; i < strlen(block) ; i++){
@@ -66,6 +74,7 @@ int calculate_block(char *block){
     return tmp;
 }
 
+//Finding block with closest sum to one with given index
 char *find_block(struct array_struct *arr_struct, int index) {
     if (index < 0 || index >= arr_struct -> blocks) return NULL;
     if (arr_struct -> array[index] == NULL) return NULL;
--- lab1/main.c
+++ lab1/main.c
@@ -4,10 +4,40 @@
 #include <memory.h>
 #include <time.h>
 #include <sys/resource.h>
+
+#define STATIC_ARRAY_SIZE 100000
+#define STATIC_BLOCK_SIZE 1000
+
+//if flag DLL is set then load library
+#ifdef DLL
+
+#include <dlfcn.h>
+
+struct array_struct{
+    char **array;
+    int blocks;
+    int block_size;
+    int is_static;
+};
+
+
+struct array_struct* (*create_struct) (int, int, int);
+void (*delete_array)(struct array_struct*);
+void (*add_block)(struct array_struct*, int, char*);
+void (*delete_block)(struct array_struct*, int);
+char* (*find_block)(struct array_struct*, int);
+
+//if not then just load lib
+#else
+
 #include "library.h"
 
+#endif
+
+//global variable used to print communicate after making function
 char *operation_print;
 
+//generate block with random chars
 char *generate_rand_block(int length){
     if (length < 1 || length >= STATIC_BLOCK_SIZE) return NULL;
     char *table = "1234567890abcdefghijklmnoprstuwvxyzABCDEFGHIJKLMNOPRSTUWVXYZ!@#$%^&*()";
@@ -20,12 +50,14 @@ char *generate_rand_block(int length){
     return block;
 }
 
+//delete blocks from start_index to start_index+number_of_blocks
 void delete_blocks(struct array_struct *arr_struct, int start_index, int number_of_blocks){
     for (int i = start_index ; i < (start_index + number_of_blocks) ; i++){
         delete_block(arr_struct,i);
     }
 }
 
+//add number_of_blocks blocks after start_index
 void add_blocks(struct array_struct *arr_struct, int start_index, int number_of_blocks, int block_size){
     for (int i = start_index ; i < start_index + number_of_blocks ; i++){
         char *tmp = generate_rand_block(block_size);
@@ -33,6 +65,7 @@ void add_blocks(struct array_struct *arr_struct, int start_index, int number_of_
     }
 }
 
+//calculate system time in function call
 float calculate_system_timediff(struct rusage *r_usage, int step_counter){
     if(step_counter == 0) {
         return (((float)(r_usage[step_counter].ru_stime.tv_sec) * 1000000) + r_usage[step_counter].ru_stime.tv_usec) / 1000000;
@@ -45,6 +78,7 @@ float calculate_system_timediff(struct rusage *r_usage, int step_counter){
     }
 }
 
+//calculate user time in function call
 float calculate_user_timediff(struct rusage *r_usage, int step_counter){
     if(step_counter == 0) {
         return (((float)(r_usage[step_counter].ru_utime.tv_sec) * 1000000) + r_usage[step_counter].ru_utime.tv_usec) / 1000000;
@@ -57,10 +91,12 @@ float calculate_user_timediff(struct rusage *r_usage, int step_counter){
     }
 }
 
+//calculate real execution time
 double calculate_real_timediff(struct timeval start, struct timeval end){
     return (((double)end.tv_sec * 1000000 + end.tv_usec)-((double) start.tv_sec* 1000000+ start.tv_usec))/1000000;
 }
 
+//prints time to console and to file
 void print_time(struct timeval real_start, struct timeval real_end, struct rusage *r_usage, FILE *file, int step_counter){
     printf("Real time: %lf    ", calculate_real_timediff(real_start, real_end));
     printf("User time: %lf    ", calculate_user_timediff(r_usage,step_counter));
@@ -70,6 +106,7 @@ void print_time(struct timeval real_start, struct timeval real_end, struct rusag
     fprintf(file, "System time: %lf    \n", calculate_system_timediff(r_usage,step_counter));
 }
 
+//function detecting and making operation
 void make_operation(struct array_struct *arrstruct, char *operation, int parameter, int block_length){
     if(strcmp(operation, "find") == 0){
         operation_print = "Finding closest block:\n";
@@ -87,9 +124,7 @@ void make_operation(struct array_struct *arrstruct, char *operation, int paramet
         for(int i = 0 ; i < parameter ; i++){
 
             delete_block(arrstruct, i);
-
             add_block(arrstruct, i, generate_rand_block(block_length));
-
         }
     }
     else {
@@ -98,11 +133,24 @@ void make_operation(struct array_struct *arrstruct, char *operation, int paramet
     }
 }
 
+
 //----------------------------------------------------------------
 
 int main(int argc, char *argv[]){
 
-    FILE *file = fopen(argv[1], "a");
+//using dlsym to sync functions
+#ifdef DLL
+    void *dynamic_handle = dlopen("./libdynamiclib.so", RTLD_LAZY);
+
+    *(void**) (&create_struct) = dlsym(dynamic_handle, "create_struct");
+    *(void**) (&delete_array) = dlsym(dynamic_handle, "delete_array");
+    *(void**) (&add_block) = dlsym(dynamic_handle, "add_block");
+    *(void**) (&delete_block) = dlsym(dynamic_handle, "delete_block");
+    *(void**) (&find_block) = dlsym(dynamic_handle, "find_block");
+#endif
+
+    FILE *file = fopen(argv[1], "a"); //opening file with a option
+
     if (file == NULL){
         printf("Cannot open the file.\n");
         return 1;
@@ -117,24 +165,26 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
+    //array struct parameters
     int array_size = (int) strtol(argv[2], NULL, 10);
     int block_length = (int) strtol(argv[3], NULL, 10);
     int is_static = (int) strtol(argv[4], NULL, 10);
 
-
-
+    //srand for random generation
     srand(time(NULL));
 
     printf("Array Size: %d   Block Length: %d   Allocation: %d\n", array_size, block_length, is_static);
     fprintf(file, "Array Size: %d   Block Length: %d   Allocation: %d\n", array_size, block_length, is_static);
 
 
+    //variables needed for time measurement
     struct timeval start, end;
-    struct rusage *r_usage = malloc(5 * sizeof(*r_usage));
-    int step_counter = 0;
+    struct rusage *r_usage = malloc(7 * sizeof(*r_usage));
+    int step_counter = 0; //variable shows where in r_usage array
 
     gettimeofday(&start, NULL);
 
+    //creating structure
     struct array_struct *arrstruct = create_struct(array_size, block_length, is_static);
     add_blocks(arrstruct,0,arrstruct -> blocks, arrstruct -> block_size);
     gettimeofday(&end, NULL);
@@ -148,7 +198,7 @@ int main(int argc, char *argv[]){
     step_counter++;
 
 
-
+    //function calls
     if(argc > 6) {
         gettimeofday(&start, NULL);
 
@@ -174,6 +224,8 @@ int main(int argc, char *argv[]){
         printf("%s", operation_print);
         fprintf(file, "%s", operation_print);
         print_time(start,end,r_usage, file, step_counter);
+
+        step_counter++;
     }
 
     if(argc > 10) {
@@ -192,4 +244,5 @@ int main(int argc, char *argv[]){
     //fclose(file);
     return 0;
 }
+
 
